7aeef6904cedd8b5aff1a8caf45875a865fe4d72
unknown <debeldamilola@gmail.com>
2020-05-21 13:53:28 +0100

    Updates

---
+++ precache-manifest.e5ec9a8e6b0362a9b5d659a8c23b47a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8f416d7905d576f9a59c95b22a0650",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9e561c00560ed7ed3c8b",
+    "url": "/monster-rolodex/static/css/main.59f0c904.chunk.css"
+  },
+  {
+    "revision": "68bafe929a46aab89f72",
+    "url": "/monster-rolodex/static/js/2.91a31895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.91a31895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e561c00560ed7ed3c8b",
+    "url": "/monster-rolodex/static/js/main.68ebc34f.chunk.js"
+  },
+  {
+    "revision": "dff50cb489e492ffd47f",
+    "url": "/monster-rolodex/static/js/runtime-main.097b1070.js"
+  }
+]);
